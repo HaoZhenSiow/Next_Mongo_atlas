5144111b0cda4cbb4e2619ae13d74f06aa9fd58a
Siow Hao Zhen <96342348+HaoZhenSiow@users.noreply.github.com>
2023-05-28 19:49:00 +0800

    tutorial complete

--- src/app/page.js
+++ src/app/page.js
@@ -10,7 +10,7 @@ export default async function Home() {
   return (
     <div className="home">
       <div className="workouts">
-        {workouts && workouts.map(workout => (
+        {workouts.map(workout => (
           <WorkoutDetails workout={workout} key={workout._id} />
         ))}
       </div>
