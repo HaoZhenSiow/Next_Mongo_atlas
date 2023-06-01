c1e91a69ab2183ff66a5c304f4a174a88ee4ad64
Siow Hao Zhen <96342348+HaoZhenSiow@users.noreply.github.com>
2023-06-01 22:23:12 +0800

    install easy-peasy

--- src/components/WorkoutDetails.js
+++ src/components/WorkoutDetails.js
@@ -1,4 +1,3 @@
-'use client'
 import axios from "axios"
 import formatDistanceToNow from 'date-fns/formatDistanceToNow'
 
---
+++ src/stores/workoutStore.js
@@ -0,0 +1,12 @@
+'use client'
+
+import { createContextStore, action } from 'easy-peasy';
+
+const WorkoutStore = createContextStore({
+  workouts: [],
+  loadWorkouts: action((state, payload) => {
+    state.workouts = payload
+  })
+});
+
+export default WorkoutStore;
