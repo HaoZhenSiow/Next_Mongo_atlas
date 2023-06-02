aafd358829b6468f5d05a7f3fa7b23116b303b82
Siow Hao Zhen <96342348+HaoZhenSiow@users.noreply.github.com>
2023-06-03 02:29:26 +0800

    make index page client component

--- src/app/page.js
+++ src/app/page.js
@@ -1,34 +1,34 @@
-// 'use client'
-// import { useEffect } from "react"
+'use client'
+import { useEffect } from "react"
 import axios from "axios"
 axios.defaults.validateStatus = false
 
 import WorkoutDetails from "@/_components/WorkoutDetails"
 import WorkoutForm from "@/_components/WorkoutForm"
 
-// import WorkoutStore from "@/stores/workoutStore"
+import WorkoutStore from "@/_stores/workoutStore"
 
-import connectDB from "@/_lib/connectDB"
-import workoutModel from "@/_models/workoutModel"
+// import connectDB from "@/_lib/connectDB"
+// import workoutModel from "@/_models/workoutModel"
 
 export default async function Home() {
-  connectDB()
-  const workoutsFetched = await workoutModel.find().sort({ createdAt: -1 })
-  const workouts = JSON.parse(JSON.stringify(workoutsFetched))
+  // connectDB()
+  // const workoutsFetched = await workoutModel.find().sort({ createdAt: -1 })
+  // const workouts = JSON.parse(JSON.stringify(workoutsFetched))
   
-  // const { workouts } = WorkoutStore.useStoreState(state => state)
-  // const { loadWorkouts } = WorkoutStore.useStoreActions(actions => actions)
-  // useEffect(() => {
-  //   const fetchWorkouts = async () => {
-  //     const { status, data } = await axios.get(process.env.NEXT_PUBLIC_WORKOUT_API)
-  //     console.log(data);
-  //     if (status === 200) {
-  //       loadWorkouts(data)
-  //     }
-  //   }
-  //   fetchWorkouts()
+  const { workouts } = WorkoutStore.useStoreState(state => state)
+  const { loadWorkouts } = WorkoutStore.useStoreActions(actions => actions)
+  useEffect(() => {
+    const fetchWorkouts = async () => {
+      const { status, data } = await axios.get(process.env.NEXT_PUBLIC_WORKOUT_API)
+      console.log(data);
+      if (status === 200) {
+        loadWorkouts(data)
+      }
+    }
+    fetchWorkouts()
     
-  // }, [loadWorkouts])
+  }, [loadWorkouts])
   
   return (
     <div className="home">
