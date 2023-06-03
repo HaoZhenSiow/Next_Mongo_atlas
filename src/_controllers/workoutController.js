3bedef8a89cc9993fad57c45fc52fc89a2b5ab2f
Siow Hao Zhen <96342348+HaoZhenSiow@users.noreply.github.com>
2023-06-04 00:18:04 +0800

    assign workouts to users

--- src/_controllers/workoutController.js
+++ src/_controllers/workoutController.js
@@ -4,22 +4,14 @@ import { res, revalidateIndex, isValidId } from '@/_lib/utils'
 export async function createWorkout(req) {
   const { origin } = new URL(req.url)
   const payload = await req.json()
-  const { title, load, reps } = payload
-  const emptyFields = []
-
-  if (!title) { emptyFields.push('title') }
-  if (!load) { emptyFields.push('load') }
-  if (!reps) { emptyFields.push('reps') }
-
-  if (emptyFields.length > 0) {
-    return res({ error: 'Please fill in all fields', emptyFields }, 400)
-  }
-
+ 
   try {
-    const new_workout = await workoutModel.create(payload)
+    const user_id = req.cookies.get('user').value
+    const new_workout = await workoutModel.create({ ...payload, user_id })
     // await revalidateIndex(origin)
     return res(new_workout, 201)
   } catch (error) {
+    console.log(error)
     return res({ error: 'Something went wrong. Please try again.' }, 500)
   }
 }
@@ -27,15 +19,18 @@ export async function createWorkout(req) {
 export async function getWorkouts(req) {
   const { searchParams } = new URL(req.url)
   const id = searchParams.get('id')
+  const user_id = req.cookies.get('user').value
+
   if (id && id.length > 0 && !isValidId(id)) {
     return res('invalid ID', 400)
   }
+
   try {
     if (isValidId(id)) {
-      const Workout = await workoutModel.findById(id)
+      const Workout = await workoutModel.find({ _id: id, user_id })
       return Workout ? res(Workout, 200) : res('no such workout', 404)
     }
-    const workouts = await workoutModel.find().sort({ createdAt: -1 })
+    const workouts = await workoutModel.find({ user_id }).sort({ createdAt: -1 })
     return res(workouts, 200)
   } catch (error) {
     return res('Something went wrong. Please try again.', 500)
