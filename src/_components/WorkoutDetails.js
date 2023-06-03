ed0c9e2d0d1373b4ed1547a854f318a76ef32b8b
Siow Hao Zhen <96342348+HaoZhenSiow@users.noreply.github.com>
2023-06-03 12:46:13 +0800

    add removeWorkout in workoutStore

--- src/_components/WorkoutDetails.js
+++ src/_components/WorkoutDetails.js
@@ -1,24 +1,28 @@
 'use client'
-import axios from "axios"
 import formatDistanceToNow from 'date-fns/formatDistanceToNow'
+import axios from "axios"
+axios.defaults.validateStatus = false
+
+import WorkoutStore from "@/_stores/workoutStore"
 
 const WorkoutDetails = ({ workout }) => {
+
+  const { removeWorkout } = WorkoutStore.useStoreActions(actions => actions)
+
   return (
     <div className="workout-details">
       <h4>{workout.title}</h4>
       <p><strong>Load (kg): </strong>{workout.load}</p>
       <p><strong>Number of reps: </strong>{workout.reps}</p>
-      {/*when the page revalidate on demand, the date might be different if use formDistancetoNow and will throw an error*/}
-      {/* <p>{formatDistanceToNow(new Date(workout.createdAt), { addSuffix: true })}</p> */}
-      <p>{workout.createdAt}</p>
+      {/*when the page revalidate on demand, the date might be different if use formDistancetoNow and will throw an error, do not use for stale content*/}
+      <p>{formatDistanceToNow(new Date(workout.createdAt), { addSuffix: true })}</p>
       <span className="material-symbols-outlined" onClick={handleDelete}>delete</span>
     </div>
   )
 
   async function handleDelete() {
-    await axios.delete(`${process.env.NEXT_PUBLIC_WORKOUT_API}?id=${workout._id}`)
-    // await axios.delete(process.env.NEXT_PUBLIC_WORKOUT_API, { params: { id: workout._id } })
-    // location.reload()
+    const { status, data } = await axios.delete(process.env.NEXT_PUBLIC_WORKOUT_API, { params: { id: workout._id } })
+    status === 200 && removeWorkout(data)
   }
 }
 
