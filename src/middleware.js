153dcaab1e125906bc52d46ba18911b25658faa5
Siow Hao Zhen <96342348+HaoZhenSiow@users.noreply.github.com>
2023-06-23 22:46:23 +0800

    adding tracker

--- src/middleware.js
+++ src/middleware.js
@@ -1,34 +1,42 @@
-import { NextResponse } from "next/server"
+import { NextRequest, NextResponse } from "next/server"
 import { decodeToken } from "./_lib/jwt"
 
 
 export async function middleware(request) {
+  console.log(request.cookies.get('ip'));
 
   const token = request.cookies.get('token') ? request.cookies.get('token') : null
-  
   const { origin, pathname } = request.nextUrl
+  
+  let response = NextResponse.next()
 
   if (!token && pathname === '/') {
-    return NextResponse.redirect(`${origin}/login`)
+    response = NextResponse.redirect(`${origin}/login`)
   }
 
   if (token && (pathname.startsWith('/login') || pathname.startsWith('/signup'))) {
-    return NextResponse.redirect(`${origin}/`)
+    response = NextResponse.redirect(`${origin}/`)
   }
   
   if (!token && pathname.startsWith('/api/workouts')) {
-    return NextResponse.json('Authorization Token required', { status: 401 })
+    response = NextResponse.json('Authorization Token required', { status: 401 })
   }
 
   if (token && pathname.startsWith('/api/workouts')) {
     try {
       await decodeToken(token.value)
-      return NextResponse.next()
+      response = NextResponse.next()
     } catch (error) {
-      return NextResponse.json('Request is not authorized', { status: 401 })
+      response = NextResponse.json('Request is not authorized', { status: 401 })
     }
   }
 
+  if (!request.cookies.get('ip')) {
+    const ip = request.headers.get('x-forwarded-for')
+    response.cookies.set('ip', ip)
+  }
+
+  return response
 }
 
 export const config = {
