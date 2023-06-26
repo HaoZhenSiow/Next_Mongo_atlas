99dc0d4134c3a7bbbae6eb8387e98453469225fb
Siow Hao Zhen <96342348+HaoZhenSiow@users.noreply.github.com>
2023-06-27 04:40:25 +0800

    disconnect DB after api access

--- src/app/api/auth/route.js
+++ src/app/api/auth/route.js
@@ -1,62 +1,78 @@
-import connectDB from "@/_lib/connectDB"
+import { connectDB, disconnectDB } from "@/_lib/connectDB"
 import userModel from "@/_models/userModel";
-import { NextResponse } from "next/server";
 import bcrypt from "bcrypt"
 import { genToken } from "@/_lib/jwt";
 import { res } from "@/_lib/utils";
 
-connectDB()
-
 export async function POST(req) {
+  await connectDB()
+
   const { request = '', email = '', password = '' } = await req.json()
+  let response = res('Invalid request.', 400)
+
   switch (request) {
     case "login":
-      return await login(email, password)
+      response = await login(email, password)
+      break
     case "signup":
-      return await signup(email, password)
+      response = await signup(email, password)
+      break
     case "logout":
-      return await logout()
-    default:
-      return res('Invalid request.', 400)
+      response = await logout()
   }
+
+  await disconnectDB()
+  return response
 }
 
 async function login(email, password) {
   try {
     const user = await userModel.findOne({ email })
     if (!user) return res('User not found.', 404)
+
     const passwordMatch = await bcrypt.compare(password, user.password)
     if (!passwordMatch) return res('Password is incorrect.', 400)
-    const token = await genToken({ id: user._id })
-    const response = NextResponse.json({ email, token }, { status: 200 })
-    response.cookies.set('token', token)
-    response.cookies.set('user', user._id)
-    return response
-  } catch (error) {
+
+    return loginSuccess(user, email)
+  }
+
+  catch (error) {
     return res('Something went wrong, please try again.', 400)
   }
 }
 
 async function signup(email, password) {
-  const salt = await bcrypt.genSalt(10)
-  const hashPassword = await bcrypt.hash(password, salt)
+  const salt = await bcrypt.genSalt(10),
+        hashPassword = await bcrypt.hash(password, salt)
+
   try {
-    const exist = await userModel.findOne({ email })
-    if (exist) return res('User already exist.', 400)
+    const existingUser = await userModel.findOne({ email })
+    if (existingUser) return res('User already exist.', 400)
+
     const user = await userModel.create({ email, password: hashPassword })
-    const token = await genToken({ id: user._id })
-    const response = NextResponse.json({ email, token }, { status: 200 })
-    response.cookies.set('token', token)
-    response.cookies.set('user', user._id)
-    return response
-  } catch (error) {
+    return loginSuccess(user, email)
+  }
+  
+  catch (error) {
     return res('Something went wrong, please try again.', 500)
   }
 }
 
 async function logout() {
-  const response = NextResponse.json('logout success', { status: 200 })
+  const response = res('logout success', 200)
+
   response.cookies.delete('token')
-  response.cookies.delete('user')
+                  .delete('user')
+
+  return response
+}
+
+async function loginSuccess(user, email) {
+  const token = await genToken({ id: user._id }),
+        response = res({ email, token }, 200)
+
+  response.cookies.set('token', token)
+                  .set('user', user._id)
+
   return response
 }
--- src/app/api/workouts/route.js
+++ src/app/api/workouts/route.js
@@ -1,20 +1,24 @@
-import connectDB from "@/_lib/connectDB"
+import { connectDB, disconnectDB } from "@/_lib/connectDB"
 import workoutModel from '@/_models/workoutModel'
 import { res, revalidateIndex, isValidId } from '@/_lib/utils'
-// import { createWorkout, getWorkouts, deleteWorkout } from "@/_controllers/workoutController"
-
-connectDB()
 
 export async function POST(req) {
-  return await createWorkout(req)
+  return await handleRequest(req, createWorkout)
 }
 
 export async function GET(req) {
-  return await getWorkouts(req)
+  return await handleRequest(req, getWorkouts)
 }
 
 export async function DELETE(req) {
-  return await deleteWorkout(req)
+  return await handleRequest(req, deleteWorkout)
+}
+
+async function handleRequest(req, callback) {
+  await connectDB()
+  const response = await callback(req)
+  await disconnectDB()
+  return response
 }
 
 async function createWorkout(req) {
@@ -22,20 +26,21 @@ async function createWorkout(req) {
   const payload = await req.json()
  
   try {
-    const user_id = req.cookies.get('user').value
-    const new_workout = await workoutModel.create({ ...payload, user_id })
+    const user_id = req.cookies.get('user').value,
+          new_workout = await workoutModel.create({ ...payload, user_id })
     // await revalidateIndex(origin)
     return res(new_workout, 201)
-  } catch (error) {
-    console.log(error)
-    return res({ error: 'Something went wrong. Please try again.' }, 500)
+  }
+
+  catch (error) {
+    return res('Something went wrong. Please try again.', 500)
   }
 }
 
 async function getWorkouts(req) {
-  const { searchParams } = req.nextUrl
-  const id = searchParams.get('id')
-  const user_id = req.cookies.get('user').value
+  const { searchParams } = req.nextUrl,
+        id = searchParams.get('id'),
+        user_id = req.cookies.get('user').value
 
   if (id && id.length > 0 && !isValidId(id)) {
     return res('invalid ID', 400)
@@ -46,25 +51,30 @@ async function getWorkouts(req) {
       const Workout = await workoutModel.find({ _id: id, user_id })
       return Workout ? res(Workout, 200) : res('no such workout', 404)
     }
+
     const workouts = await workoutModel.find({ user_id }).sort({ createdAt: -1 })
     return res(workouts, 200)
-  } catch (error) {
+  }
+  
+  catch (error) {
     return res('Something went wrong. Please try again.', 500)
   }
 }
 
 async function deleteWorkout(req) {
-  const { origin, searchParams } = req.nextUrl
-  const id = searchParams.get('id')
-  if (!isValidId(id)) {
-    return res('invalid ID', 400)
-  }
+  const { origin, searchParams } = req.nextUrl,
+        id = searchParams.get('id')
+
+  if (!isValidId(id)) return res('invalid ID', 400)
+  
   try {
     const Workout = await workoutModel.findByIdAndDelete(id)
     if (!Workout) return res('no such workout', 404)
     // await revalidateIndex(origin)
     return res(Workout, 200)
-  } catch (error) {
+  }
+  
+  catch (error) {
     return res('Something went wrong. Please try again.', 500)
   }
 }
