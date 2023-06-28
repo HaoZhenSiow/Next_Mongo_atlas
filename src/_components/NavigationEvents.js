2fec6be4d1434c8b82dc0fdbe9d69f72248e787f
Siow Hao Zhen <96342348+HaoZhenSiow@users.noreply.github.com>
2023-06-29 06:27:39 +0800

    tracker capture events and duration

--- src/_components/NavigationEvents.js
+++ src/_components/NavigationEvents.js
@@ -2,6 +2,7 @@
 
 import { useEffect } from "react"
 import { usePathname } from 'next/navigation'
+import Bowser from "bowser"
 import axios from "axios"
 axios.defaults.validateStatus = false
 
@@ -9,6 +10,9 @@ const NavigationEvents = () => {
   const pathname = usePathname()
 
   useEffect(() => {
+    // const browser = Bowser.getParser(window.navigator.userAgent)
+    // const browser2 = navigator.userAgentData.brands[2].brand
+    // console.log(browser.getBrowser(), browser2)
     async function fetcher() {
       await axios.post('/api/tracker/', {
         path: pathname
