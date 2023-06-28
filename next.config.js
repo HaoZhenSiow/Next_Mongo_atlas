3f7086762b5047bafceff54a423ce150ee505f47
Siow Hao Zhen <96342348+HaoZhenSiow@users.noreply.github.com>
2023-06-29 06:27:39 +0800

    tracker capture events and duration

--- next.config.js
+++ next.config.js
@@ -1,21 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  async redirects() {
-    return [
-      {
-        source: '/',
-        missing: [
-          {
-            type: 'cookie',
-            key: 'token'
-          }
-        ],
-        destination: '/login',
-        permanent: false
-      }
-    ]
-  }
 }
 
 module.exports = nextConfig
