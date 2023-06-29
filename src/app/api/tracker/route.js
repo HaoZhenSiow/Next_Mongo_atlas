a0c00e1d5940d51557b543c13f003179fc75ef8c
Siow Hao Zhen <96342348+HaoZhenSiow@users.noreply.github.com>
2023-06-30 06:05:07 +0800

    capture browser, platform & resolution for tracker

--- src/app/api/tracker/route.js
+++ src/app/api/tracker/route.js
@@ -6,27 +6,23 @@ import { res } from "@/_lib/utils"
 connectDB()
 
 export async function POST(req) {
-  const { path = '' } = await req.json()
+  const body = await req.json()
   const ip = req.headers.get('x-forwarded-for')
   const uid = req.cookies.get('uid')
 
-  // if (uid) {
-  //   return await continueSession(path, ip, uid.value)
-  // }
-  // console.log(uid);
-  // return res('done', 200)
-  return await createSession(path, ip)
+  if (uid) {
+    return await continueSession(uid.value, body)
+  }
+  
+  return await createSession(ip, body)
 }
 
-async function createSession(path, ip) {
+async function createSession(ip, body) {
   try {
     const session = await sessionModel.create({
       uid: new mongoose.Types.ObjectId(),
       ip,
-      path: [{
-        event: path,
-        duration: 0
-      }]
+      events: [body]
     })
     
     const response = res('created new session', 200)
@@ -40,27 +36,22 @@ async function createSession(path, ip) {
   
 }
 
-async function continueSession(path, ip, uid) {
+async function continueSession(uid, body) {
   try {
     const session = await sessionModel.findOne({ uid })
     if (!session) throw new Error('Session not found')
 
-    const prevEvent = session.path.at(-1)
-    if (prevEvent.event.startsWith('/') && prevEvent.event === path) {
-      return res(session, 200)
-    }
+    const prevEvent = session.events.at(-1),
+          isPageEvent = prevEvent.event.startsWith('/'),
+          isRefreshEvent = prevEvent.event === body.event
+    if (isPageEvent && isRefreshEvent) return res('refresh page', 200)
     
     const prevEventDuration = new Date() - prevEvent.createdAt
-    const newEvent = {
-      event: path,
-      duration: 0
-    }
-
     prevEvent.duration = prevEventDuration
-    session.path.push(newEvent)
+    session.events.push(body)
     await session.save()
 
-    const response = res(session, 200)
+    const response = res('added new event', 200)
     response.cookies.set('uid', session.uid)
     return response
   }
