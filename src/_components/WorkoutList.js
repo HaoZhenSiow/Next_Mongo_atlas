34ed2bfc32fc3e905949157ccfbb54fd5f59b0ce
Siow Hao Zhen <96342348+HaoZhenSiow@users.noreply.github.com>
2023-07-02 01:32:14 +0800

    edit main page

--- src/_components/WorkoutList.js
+++ src/_components/WorkoutList.js
@@ -3,7 +3,6 @@ import { useEffect } from "react"
 import axios from "axios"
 axios.defaults.validateStatus = false
 
-import AuthStore from '@/_stores/authStore'
 import WorkoutDetails from "@/_components/WorkoutDetails"
 
 import WorkoutStore from "@/_stores/workoutStore"
@@ -11,20 +10,14 @@ import WorkoutStore from "@/_stores/workoutStore"
 export default function WorkoutList(props) {
   const { workouts } = WorkoutStore.useStoreState(state => state)
   const { loadWorkouts } = WorkoutStore.useStoreActions(actions => actions)
-  const { logout } = AuthStore.useStoreActions(actions => actions)
 
   useEffect(() => {
     async function fetchWorkouts() {
       const { status, data } = await axios.get(process.env.NEXT_PUBLIC_WORKOUT_API)
-      switch (status) {
-        case 200:
-          loadWorkouts(data)
-          break
-        case 401:
-          alert('You token is unauthorized, please log in again')
-          logout()
-          break
+      if (status === 200) {
+        loadWorkouts(data)
       }
+      
     }
     fetchWorkouts()
   }, [loadWorkouts])
