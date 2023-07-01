27b57f99b5ff1d1ff6fcbd5873f1fca35db13dc2
Siow Hao Zhen <96342348+HaoZhenSiow@users.noreply.github.com>
2023-07-02 01:37:51 +0800

    add await to loginSuccess

--- src/_components/WorkoutList.js
+++ src/_components/WorkoutList.js
@@ -3,6 +3,7 @@ import { useEffect } from "react"
 import axios from "axios"
 axios.defaults.validateStatus = false
 
+import AuthStore from '@/_stores/authStore'
 import WorkoutDetails from "@/_components/WorkoutDetails"
 
 import WorkoutStore from "@/_stores/workoutStore"
@@ -10,14 +11,20 @@ import WorkoutStore from "@/_stores/workoutStore"
 export default function WorkoutList(props) {
   const { workouts } = WorkoutStore.useStoreState(state => state)
   const { loadWorkouts } = WorkoutStore.useStoreActions(actions => actions)
+  const { logout } = AuthStore.useStoreActions(actions => actions)
 
   useEffect(() => {
     async function fetchWorkouts() {
       const { status, data } = await axios.get(process.env.NEXT_PUBLIC_WORKOUT_API)
-      if (status === 200) {
-        loadWorkouts(data)
+      switch (status) {
+        case 200:
+          loadWorkouts(data)
+          break
+        case 401:
+          alert('You token is unauthorized, please log in again')
+          logout()
+          break
       }
-      
     }
     fetchWorkouts()
   }, [loadWorkouts])
